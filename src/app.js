386018f2c35b8ebef0722d6474e1ecac4e665321
Julio Cesar <juliozulu2008@gmail.com>
2025-06-15 23:58:33 -0300

    Craindo Api com os padroes de pastas e de codigos corretos, vendo algumas configuraçoes para codido e editor de codigo vscode tempo 03:02

--- src/app.js
+++ src/app.js
@@ -2,24 +2,25 @@ import express from "express";
 import routes from "./routes"; // Assuming you have a routes file
 
 class App {
-  /**
-   * Creates an instance of the App class.
-   * Initializes the Express server.
-   */
-  constructor() {
-    this.server = express();
-    this.middlewares();
-    this.routes();
-  }
+    /**
+     * Creates an instance of the App class.
+     * Initializes the Express server.
+     */
+    constructor() {
+        this.server = express();
+        this.middlewares();
+        this.routes();
+    }
 
-  middlewares() {
-    // Middleware to parse JSON request bodies
-    this.server.use(express.json());
-  }
+    middlewares() {
+        // Middleware to parse JSON request bodies
+        this.server.use(express.json());
+    }
 
-  routes() {
-    this.server.use(routes);
-  }
+    routes() {
+        this.server.use(routes);
+    }
 }
 
 export default new App().server; // Export the server instance
+// just to commite
